7bc81a6df56344e0eb0153d01c396513dd080617
Itallo Kavin <italo.keven50@gmail.com>
2022-06-23 11:19:27 -0300

    Update

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,6 @@
-alert('Oi');
+const fs = require('fs');
 
-import { readFile } from 'fs';
-let getJson = readFile('./procedimentos.json','utf-8');
+const getJson = fs.readFileSync('./procedimentos.json','utf-8');
+let dados = JSON.parse(getJson);
+console.log(dados[0].Codigo);
 
-getJson = JSON.parse(getJson);
-
-let cod = document.getElementById('codigo');
-let cat = document.getElementById('categoria');
-let des = document.getElementById('descricao');
-
-let key = 0;
-
-while(key <= getJson.length){
-  cod.append(`${getJson[key].Codigo}`);
-  cat.append(`${getJson[key].Categoria}`);
-  des.append(`${getJson[key].Descricao}`);
-  id++
-}
--- scripts/require.js
+++
@@ -1,7 +0,0 @@
-require(['scripts/config'], function() {
-    // Configuration loaded now, safe to do other require calls
-    // that depend on that config.
-    require(['foo'], function(foo) {
-
-    });
-});
