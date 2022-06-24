de68aff2351a5c06a0164849caa5a104c78e5049
Itallo Kavin <italo.keven50@gmail.com>
2022-06-24 15:30:19 -0300

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function fazGet(url) {
 }
 
 function criaLinha(procedimentos){
-    
+    document.querySelector('.num').innerHTML = pageNum
     linha = document.createElement("tr");
     tdCategoria = document.createElement ("td");
     tdDescricao = document.createElement ("td");
@@ -17,13 +17,12 @@ function criaLinha(procedimentos){
     tdCategoria.innerHTML = procedimentos.Categoria
     tdDescricao.innerHTML = procedimentos.Descricao
     tdCodigo.innerHTML = procedimentos.Codigo
-
+    
     linha.appendChild(tdCodigo);
     linha.appendChild(tdCategoria);
     linha.appendChild(tdDescricao);
-   
+    
     return linha;
-
 }
 
 function main(url){
@@ -32,12 +31,80 @@ function main(url){
     let tabela = document.getElementById("tabela");
     procedimentos.forEach(element => {  
     let linha = criaLinha(element);
-    
+
     tabela.appendChild(linha);
 
     });
 
 }
+
+const html = {
+    get(element){
+        return document.querySelector(element)
+    }
+}
+
+let totalDados = JSON.parse(fazGet('http://localhost:3000/procedimentos')).length
+let totalPage = Math.ceil(totalDados / 15)
+let pageNum = 1
+
+const controls = {
+    next(){
+        tabela.innerHTML = "";
+        pageNum++
+        if(pageNum > totalPage){
+            pageNum--
+            main(`http://localhost:3000/procedimentos/?_page=${pageNum}&_limit=15`)
+        }else{
+            main(`http://localhost:3000/procedimentos/?_page=${pageNum}&_limit=15`)
+        }
+        console.log(pageNum)
+    },
+    prev(){
+        tabela.innerHTML = "";
+        pageNum--
+        if(pageNum < 1){
+            pageNum++
+            main(`http://localhost:3000/procedimentos/?_page=${pageNum}&_limit=15`)
+        }
+        else{
+            main(`http://localhost:3000/procedimentos/?_page=${pageNum}&_limit=15`)
+        }
+        console.log(pageNum)
+    },
+    last(){
+        tabela.innerHTML = "";
+        pageNum = totalPage
+        main(`http://localhost:3000/procedimentos/?_page=${pageNum}&_limit=15`)
+        console.log(pageNum)
+    },
+    first(){
+        tabela.innerHTML = "";
+        pageNum = 1
+        main(`http://localhost:3000/procedimentos/?_page=${pageNum}&_limit=15`)
+        console.log(pageNum)
+    },
+    createListeners(){
+        html.get('.next').addEventListener('click',() =>{
+            controls.next()
+            
+        });
+        html.get('.prev').addEventListener('click',() =>{
+            controls.prev()
+            
+        });
+        html.get('.last').addEventListener('click',() =>{
+            controls.last()
+            
+        });
+        html.get('.first').addEventListener('click',() =>{
+            controls.first()
+            
+        });
+    }
+}
+
+controls.createListeners();
 
 function makeRequest(queryParam){
     let tabela = document.getElementById("tabela");
@@ -45,7 +112,8 @@ function makeRequest(queryParam){
     if(queryParam){
         main('http://localhost:3000/procedimentos?Categoria=' + queryParam)
     }else{
-        main('http://localhost:3000/procedimentos')
+        main(`http://localhost:3000/procedimentos/?_page=${pageNum}&_limit=15`)
     }
 }
-window.onload = main('http://localhost:3000/procedimentos')
+window.onload = main(`http://localhost:3000/procedimentos/?_page=${pageNum}&_limit=15`)
+
