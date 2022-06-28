8ea02a723655a9a50beb400ea69304f043fe9f47
Itallo Kavin <italo.keven50@gmail.com>
2022-06-28 17:05:26 -0300

    Adicionada a função de listar a quantidade de resgistros por pagina.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function fazGet(url) {
     let request = new XMLHttpRequest()
     request.open("Get", url , false)
@@ -7,7 +8,7 @@ function fazGet(url) {
 }
 
 function criaLinha(procedimentos){
-    document.querySelector('.num').innerHTML = pageNum
+    document.querySelector('.num').innerHTML = pageAtual
     linha = document.createElement("tr");
     tdCategoria = document.createElement ("td");
     tdDescricao = document.createElement ("td");
@@ -37,18 +38,22 @@ function main(url){
     });
 
 }
-let btn = document.getElementsByClassName("btn")
 
-for (var i = 0; i < btn.length; i++) {
-  btn[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  if (current.length > 0) { 
-    current[0].className = current[0].className.replace(" active", "");
-    
-  }
-  this.className += " active";
-    
-  });
+function pressBtnCategory(){
+    let btn = document.getElementsByClassName("btn")
+
+    for (var i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    if (current.length > 0) { 
+        current[0].className = current[0].className.replace(" active", "");
+        
+    }
+    this.className += " active";
+        
+    });
+    }
+
 }
 
 const html = {
@@ -57,75 +62,15 @@ const html = {
     }
 }
 
-let totalDados = JSON.parse(fazGet('http://localhost:3000/procedimentos')).length
-let totalPage = Math.ceil(totalDados / 15)
-let pageNum = 1
-
-const controls = {
-    next(){
-        tabela.innerHTML = "";
-        pageNum++
-        if(pageNum > totalPage){
-            pageNum--
-            main(`http://localhost:3000/procedimentos/?_page=${pageNum}&_limit=13`)
-        }else{
-            main(`http://localhost:3000/procedimentos/?_page=${pageNum}&_limit=13`)
-        }
-        console.log(pageNum)
-    },
-    prev(){
-        tabela.innerHTML = "";
-        pageNum--
-        if(pageNum < 1){
-            pageNum++
-            main(`http://localhost:3000/procedimentos/?_page=${pageNum}&_limit=13`)
-        }
-        else{
-            main(`http://localhost:3000/procedimentos/?_page=${pageNum}&_limit=13`)
-        }
-        console.log(pageNum)
-    },
-    last(){
-        tabela.innerHTML = "";
-        pageNum = totalPage
-        main(`http://localhost:3000/procedimentos/?_page=${pageNum}&_limit=13`)
-        console.log(pageNum)
-    },
-    first(){
-        tabela.innerHTML = "";
-        pageNum = 1
-        main(`http://localhost:3000/procedimentos/?_page=${pageNum}&_limit=13`)
-        console.log(pageNum)
-    },
-    createListeners(){
-        html.get('.next').addEventListener('click',() =>{
-            controls.next()
-            
-        });
-        html.get('.prev').addEventListener('click',() =>{
-            controls.prev()
-            
-        });
-        html.get('.last').addEventListener('click',() =>{
-            controls.last()
-            
-        });
-        html.get('.first').addEventListener('click',() =>{
-            controls.first()
-            
-        });
-    }
-}
-
-controls.createListeners();
-
 function makeRequest(queryParam){
     let tabela = document.getElementById("tabela");
     tabela.innerHTML = ''
     if(queryParam){
-        main(`http://localhost:3000/procedimentos?Categoria=` + queryParam + `&_page=1&_limit=13`)
+        main(`http://localhost:3000/procedimentos?Categoria=` + queryParam + `&_page=1&_limit=${limitPage}`)
+        
     }else{
-        main(`http://localhost:3000/procedimentos/?_page=${pageNum}&_limit=13`)
+        main(`http://localhost:3000/procedimentos/?_page=${pageAtual}&_limit=13`)
+    
     }
 }
 
@@ -133,11 +78,95 @@ function filterSearch(){
     html.get('#search').addEventListener('keydown',()=>{
         const dataSearchKey = document.querySelector('#search').value
         tabela.innerHTML = ""
-        main(`http://localhost:3000/procedimentos/?_page=1&_limit=13&Descricao_like=${dataSearchKey}`)       
+        main(`http://localhost:3000/procedimentos/?_page=1&_limit=${limitPage}&Descricao_like=${dataSearchKey}`)       
         
     })
 }
-filterSearch()
 
-window.onload = main(`http://localhost:3000/procedimentos/?_page=${pageNum}&_limit=13`)
+const totalDados = JSON.parse(fazGet('http://localhost:3000/procedimentos')).length /*retorna total de dados json, 314 registros*/
+const limitPage = 13 /* Limite de registros por página */
+const totalPage = Math.ceil(totalDados / limitPage) /* retorna total de páginas com limite de registros pro página. */
+const pageIni = 1 /* Define a página INICIAL*/
+let pageAtual = 1 /* Inicia a o contador da página inicial */
+
+function registro(paginaAtual){
+    let exibirRegistro = paginaAtual * limitPage
+    if (exibirRegistro > totalDados ){
+        exibirRegistro = totalDados
+    }
+    if(exibirRegistro < limitPage){
+        exibirRegistro = limitPage
+    }
+    document.getElementById("totalRegistro").innerHTML = `Exibindo ${exibirRegistro} de ${totalDados} registros.`
+    
+}
+
+const pagination = {
+    next(){
+        tabela.innerHTML = "";
+        pageAtual++
+        if(pageAtual > totalPage){
+            pageAtual--
+            main(`http://localhost:3000/procedimentos/?_page=${pageAtual}&_limit=13`)
+        }else{
+            main(`http://localhost:3000/procedimentos/?_page=${pageAtual}&_limit=13`)
+            registro(pageAtual)
+        }
+    },
+    prev(){
+        tabela.innerHTML = "";
+        pageAtual--
+        if(pageAtual < pageIni){
+            pageAtual++
+            main(`http://localhost:3000/procedimentos/?_page=${pageAtual}&_limit=13`)
+        }else{
+            main(`http://localhost:3000/procedimentos/?_page=${pageAtual}&_limit=13`)
+            registro(pageAtual)
+        }
+    },
+    last(){
+        tabela.innerHTML = "";
+        pageAtual = totalPage
+        if(pageAtual > totalPage){
+            pageAtual--
+            main(`http://localhost:3000/procedimentos/?_page=${pageAtual}&_limit=13`)
+        }else{
+            main(`http://localhost:3000/procedimentos/?_page=${pageAtual}&_limit=13`)
+            registro(pageAtual)
+        }
+    },
+    first(){
+        tabela.innerHTML = "";
+        pageAtual = pageIni
+        if(pageAtual < pageIni){
+            pageAtual++
+            main(`http://localhost:3000/procedimentos/?_page=${pageAtual}&_limit=13`)
+        }else{
+            main(`http://localhost:3000/procedimentos/?_page=${pageAtual}&_limit=13`)
+            registro(pageAtual)
+        }
+    },
+    createListeners(){
+        html.get('.next').addEventListener('click',() =>{
+            pagination.next()
+        });
+        html.get('.prev').addEventListener('click',() =>{
+            pagination.prev()
+        })
+        html.get('.last').addEventListener('click',() =>{
+            pagination.last()
+        })
+        html.get('.first').addEventListener('click',() =>{
+            pagination.first()
+        })
+    }
+}
+
+pagination.createListeners();
+filterSearch();
+pressBtnCategory();
+
+window.onload = main(`http://localhost:3000/procedimentos/?_page=1&_limit=13`)
+registro(pageAtual)
+
 
